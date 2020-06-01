49352eb5251b96a7f3d6b9a68b81dbd1388dfbde
vinnom <vinnom.2d@gmail.com>
2020-06-01 16:15:05 -0300

    commit inicial

---
+++ app/src/main/java/com/example/alurafood/FormularioCadastroActivity.java
@@ -0,0 +1,14 @@
+package com.example.alurafood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_formulario_cadastro);
+	}
+}
